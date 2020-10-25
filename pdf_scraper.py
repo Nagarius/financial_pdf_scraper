dc9d5ccebc8efe987ca5cb1d2d43cb308c7527bd
Nagarius <pooper2010@live.com.au>
2020-10-25 18:22:33 +1000

    partially working with sydney airport annual report

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -1,9 +1,37 @@
 import tabula
+import fitz
 
 print('hello')
 
+#financial_report1 = "SYD_Annual_Report_2019_FINAL.pdf"
+financial_report = "SYD_Annual_Report_2019_FINAL.pdf"
+financial_report1 = "cba-2020-annual-report.pdf"
+
+#### search term ####
+search_term = ["income", "profit", "income tax", "expenses", "operating", "year ended"]
+pdf_document = fitz.open(financial_report)
+pages_wterm = []
+
+print(len(pdf_document))
+
+iffound = False
+for current_page in range(len(pdf_document)):
+    page = pdf_document.loadPage(current_page)
+    for index in range(len(search_term)):
+        if page.searchFor(search_term[index]):
+            iffound = True
+        else:
+            iffound = False
+            break
+    if(iffound):
+        pages_wterm.append(current_page + 1)
+        iffound = False
+print(pages_wterm)        
+   
+
+
 # Read pdf int list of DataFrame
-df = tabula.read_pdf("SYD_Annual_Report_2019_FINAL.pdf", pages='1')
+df = tabula.read_pdf(financial_report, pages= pages_wterm )
 
 # convert PDF into CSV file
-tabula.convert_into("SYD_Annual_Report_2019_FINAL.pdf", "output.csv", output_format="csv", pages='all')
+tabula.convert_into(financial_report, "output.csv", output_format="csv", pages= pages_wterm)
